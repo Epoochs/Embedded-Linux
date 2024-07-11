dacb8d33acf9e322d3eee2cc995607e35ddc42e4
Ahmed Sherif <88292509+Epoochs@users.noreply.github.com>
2024-07-12 00:44:29 +0300

    Update and rename BashScript.bash to file_organizer.bash

--- Makefile/Task 2 (Bash)/file_organizer.bash
+++ Makefile/Task 2 (Bash)/file_organizer.bash
@@ -1,15 +1,19 @@
 #!/bin/bash
 # This script is used to sort the files inside the src dir according to its type
 
-cd src; mkdir images documents others
+# It will create a new folder for each type of file and move the files inside the src dir
+cd src; mkdir "images" "documents" "others"
+
 for file in *
 do
     if [[ $file == *.jpg  || $file == *.png || $file == *.gif ]] ;then
-        mv "$file" images
+        mv "$file" "images"
         elif [[ $file == *.txt || $file == *.doc || $file == *.pdf ]] ;then
-        mv "$file" documents
+        mv "$file" "documents"
     else
-        mv "$file" others
+        if ! [ -d "$file" ]; then
+            mv "$file" "others"
+        fi
     fi
 done
 
